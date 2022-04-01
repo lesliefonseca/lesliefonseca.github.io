3c77f3fff40ff5fefc61725e2675743ae4136cfc
lesliefonseca <fonsl425@newschool.edu>
2022-04-01 11:30:23 -0400

    updated
    updated

--- homework/homeworkfive/script.js
+++ homework/homeworkfive/script.js
@@ -1,19 +1,19 @@
-
 function getInfo() {
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(function(response) {
 return response.json();
 })
 .then((response) => {
-console.log(response);
+
 var text="latitude";
 response.forEach(element=> {
+
 var tag = document.createElement ("p");
 var text = document.createTextNode (element.name);
 if (element.address.geo.lat < -30) {
     tag.style.fontFamily = "courier new";
 }
-var el = document.getElementById("people");
+var el = document.getElementById("people-names");
 tag.appendChild(text);
 el.appendChild(tag);
 document.querySelector ("ul")
